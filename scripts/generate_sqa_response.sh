a354c6a6ec1ae42f1857dc71a2f1e18f035698a9
royalneverwin <1900011102@pku.edu.cn>
2026-01-29 21:17:15 +0800

    update

--- scripts/generate_sqa_response.sh
+++ scripts/generate_sqa_response.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICES=0 python -m llava.eval.model_vqa_science \
-    --model-path <model-path> \
-    --question-file /<path>/ScienceQA/data/scienceqa/llava_test_QCM-LEPA.json \
-    --image-folder /<path>/ScienceQA/data/scienceqa/images/test \
-    --question-file-calibrate /<path>/ScienceQA/data/scienceqa/llava_train_QCM-LEPA.json \
-    --image-folder-calibrate /<path>/ScienceQA/data/scienceqa/images/train \
-    --answers-file /<path>/LLaVA/results/ScienceQA/LLaVA-vicuna-7B-v1.3-4bit.jsonl \
+    --model-path /data1/public_data/llava_ckpt/LLaVA-vicuna-7B-v1.3-ScienceQA \
+    --question-file /home/wangxinhao/ScienceQA/data/scienceqa/llava_test_QCM-LEPA.json \
+    --image-folder /home/wangxinhao/ScienceQA/data/scienceqa/images/test \
+    --question-file-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/llava_train_QCM-LEPA.json \
+    --image-folder-calibrate /home/wangxinhao/data/scienceqa/images/train \
+    --answers-file /home/wangxinhao/QVLM/results/ScienceQA/LLaVA-vicuna-7B-v1.3-4bit-chunk$CHUNKS_$IDX.jsonl \
     --conv-mode llava_v1 \
     --load-4bit
