eed475684a85f3b8b78ebdef766bf0b8d81865d3
royalneverwin <1900011102@pku.edu.cn>
2026-01-29 21:24:08 +0800

    update

--- scripts/generate_sqa_response_multi.sh
+++ scripts/generate_sqa_response_multi.sh
@@ -2,10 +2,10 @@ CHUNKS=8
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=$((IDX+0)) python -m llava.eval.model_vqa_science \
     --model-path /data1/public_data/llava_ckpt/LLaVA-vicuna-7B-v1.3-ScienceQA \
-    --question-file /home/wangxinhao/ScienceQA/data/scienceqa/llava_test_QCM-LEPA.json \
+    --question-file /home/wangxinhao/ScienceQA/data/scienceqa/llava_test_QCM-LEA.json \
     --image-folder /home/wangxinhao/ScienceQA/data/scienceqa/images/test \
-    --question-file-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/llava_train_QCM-LEPA.json \
-    --image-folder-calibrate /home/wangxinhao/data/scienceqa/images/train \
+    --question-file-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/llava_train_QCM-LEA.json \
+    --image-folder-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/images/train \
     --answers-file /home/wangxinhao/QVLM/results/ScienceQA/LLaVA-vicuna-7B-v1.3-4bit-chunk$CHUNKS_$IDX.jsonl \
     --num-chunks $CHUNKS \
     --chunk-idx $IDX \
