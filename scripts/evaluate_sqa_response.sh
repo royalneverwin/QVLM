a8670868e817368f819c23c1a4b9b205dd667d70
royalneverwin <1900011102@pku.edu.cn>
2026-01-30 14:07:08 +0800

    update

--- scripts/evaluate_sqa_response.sh
+++ scripts/evaluate_sqa_response.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python ../llava/eval/eval_science_qa.py \
-    --base-dir /<path>/ScienceQA/data/scienceqa \
-    --result-file /<path>/LLaVA/results/ScienceQA/LLaVA-vicuna-7B-v1.3-4bit.jsonl \
-    --output-file /<path>/LLaVA/results/ScienceQA/test_llava-7b_output.json \
-    --output-result /<path>/LLaVA/results/ScienceQA/test_llava-7b_result.json 
+CUDA_VISIBLE_DEVICES=0 python llava/eval/eval_science_qa.py \
+    --base-dir /home/wangxinhao/ScienceQA/data/scienceqa \
+    --result-file /home/wangxinhao/QVLM/results/ScienceQA/LLaVA-vicuna-7B-v1.3-4bit.jsonl \
+    --output-file /home/wangxinhao/QVLM/results/ScienceQA/test_llava-7b_output.json \
+    --output-result /home/wangxinhao/QVLM/results/ScienceQA/test_llava-7b_result.json 
