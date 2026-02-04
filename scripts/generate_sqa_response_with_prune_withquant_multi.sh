ef67ca7f5036a4aff9d7b1b300c2ce7588388d61
royalneverwin <1900011102@pku.edu.cn>
2026-02-04 22:36:40 +0800

    update

---
+++ scripts/generate_sqa_response_with_prune_withquant_multi.sh
@@ -0,0 +1,17 @@
+CHUNKS=8
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=$((IDX+0)) python -m llava.eval.model_vqa_science \
+    --model-path /data1/public_data/llava_ckpt/LLaVA-vicuna-7B-v1.3-ScienceQA \
+    --question-file /home/wangxinhao/ScienceQA/data/scienceqa/llava_test_QCM-LEA.json \
+    --image-folder /home/wangxinhao/ScienceQA/data/scienceqa/images/test \
+    --question-file-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/llava_train_QCM-LEA.json \
+    --image-folder-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/images/train \
+    --answers-file /home/wangxinhao/QVLM/results/ScienceQA_Prune_128_withquant/LLaVA-vicuna-7B-v1.3-4bit-test400-chunk$CHUNKS_$IDX.jsonl \
+    --num-chunks $CHUNKS \
+    --chunk-idx $IDX \
+    --load-4bit \
+    --visual_token_num 128 \
+    --add_quant \
+    --test_len 50 \
+    --conv-mode llava_v1  &
+done
