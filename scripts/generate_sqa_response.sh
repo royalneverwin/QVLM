443e1b77c3152e6a9917d5373b17340bcfed7267
wangxinhao.0206 <wangxinhao.0206@bytedance.com>
2026-03-03 19:51:33 +0800

    update

--- scripts/generate_sqa_response.sh
+++ scripts/generate_sqa_response.sh
@@ -1,9 +1,9 @@
 CUDA_VISIBLE_DEVICES=7 python -m llava.eval.model_vqa_science \
-    --model-path /data1/public_data/llava_ckpt/LLaVA-vicuna-7B-v1.3-ScienceQA \
-    --question-file /home/wangxinhao/ScienceQA/data/scienceqa/llava_test_QCM-LEA.json \
-    --image-folder /home/wangxinhao/ScienceQA/data/scienceqa/images/test \
-    --question-file-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/llava_train_QCM-LEA.json \
-    --image-folder-calibrate /home/wangxinhao/ScienceQA/data/scienceqa/images/train \
-    --answers-file /home/wangxinhao/QVLM/results/tmp/LLaVA-vicuna-7B-v1.3-4bit.jsonl \
+    --model-path /mnt/bn/yufei1900/wangxinhao/paper/checkpoint/LLaVA-vicuna-7B-v1.3-ScienceQA \
+    --question-file /mnt/bn/yufei1900/wangxinhao/paper/ScienceQA/data/scienceqa/llava_test_QCM-LEA.json \
+    --image-folder /mnt/bn/yufei1900/wangxinhao/paper/ScienceQA/data/scienceqa/images/test \
+    --question-file-calibrate /mnt/bn/yufei1900/wangxinhao/paper/ScienceQA/data/scienceqa/llava_train_QCM-LEA.json \
+    --image-folder-calibrate /mnt/bn/yufei1900/wangxinhao/paper/ScienceQA/data/scienceqa/images/train \
+    --answers-file /mnt/bn/yufei1900/wangxinhao/paper/QVLM/results/tmp/LLaVA-vicuna-7B-v1.3-4bit.jsonl \
     --conv-mode llava_v1 \
     --load-4bit
