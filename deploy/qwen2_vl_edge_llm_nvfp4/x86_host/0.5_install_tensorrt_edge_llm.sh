4c2e10c74148c9fd7de8818b4b73eaf26079fad8
royalneverwin <1900011102@pku.edu.cn>
2026-05-20 16:26:22 +0000

    update

---
+++ deploy/qwen2_vl_edge_llm_nvfp4/x86_host/0.5_install_tensorrt_edge_llm.sh
@@ -0,0 +1,7 @@
+# 1. 克隆仓库
+git clone https://github.com/NVIDIA/TensorRT-Edge-LLM.git
+cd TensorRT-Edge-LLM
+git submodule update --init --recursive
+
+# 2. pip 安装 Python 包（会提供那三个 CLI 命令）
+pip3 install .
